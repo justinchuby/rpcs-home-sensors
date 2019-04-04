cbd71679b1910127886a0d1823bce6377654914a
justinchuby <justinchuby@users.noreply.github.com>
2019-04-04 00:10:55 -0400

    Rename pixels_test to thermal_cam

--- playground/thermal_cam/thermal_cam.ino
+++ playground/thermal_cam/thermal_cam.ino
@@ -24,23 +24,11 @@ Adafruit_AMG88xx amg;
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println(F("AMG88xx pixels"));
-
+    Serial.begin(115200);
     bool status;
     
     // default settings
     status = amg.begin();
-    if (!status) {
-        Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
-        while (1);
-    }
-    
-    Serial.println("-- Pixels Test --");
-
-    Serial.println();
-
-    delay(100); // let sensor boot up
 }
 
 
@@ -48,15 +36,13 @@ void loop() {
     //read all the pixels
     amg.readPixels(pixels);
 
-    Serial.print("[");
-    for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
-      Serial.print(pixels[i-1]);
-      Serial.print(", ");
-      if( i%8 == 0 ) Serial.println();
+    for(int i=0; i<AMG88xx_PIXEL_ARRAY_SIZE; i++){
+      Serial.print(pixels[i]);
+      Serial.print(",");
     }
-    Serial.println("]");
+    // End each frame with a linefeed
     Serial.println();
-
-    //delay a second
-    delay(1000);
+  
+    // Give Processing time to chew
+    delay(100);
 }
