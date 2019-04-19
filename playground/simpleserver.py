260357d12697f8ae01133e08d4f2ddf0bbecdfa1
justinchuby <justinchuby@users.noreply.github.com>
2019-04-19 17:07:20 -0400

    Create simpleserver.py

---
+++ playground/simpleserver.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/hs', methods=('POST', 'GET'))
+def main():
+    if request.method == 'GET':
+        return 'You GOT this'
+
+    print(request.method, request.get_json())
+    return ''
+
+if __name__ == '__main__':
+    app.run()
