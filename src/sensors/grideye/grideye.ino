54a95f8277c6678caa2df8e4b449815a9f80b558
justinchuby <justinchuby@users.noreply.github.com>
2019-04-21 13:58:56 -0400

    Snapshot

--- src/sensors/grideye/grideye.ino
+++ src/sensors/grideye/grideye.ino
@@ -1,13 +1,20 @@
 #include <Adafruit_AMG88xx.h>
 #include <Arduino.h>
-#include <WiFi.h>
 #include <HSJsonConnector.h>
+#include <WiFi.h>
 #include "config.h"
+
+#define STOVE_HOT_TEMP 50.0
+#define STOVE_COLD_TEMP 40.0
+#define MOVING_AVG_SIZE 10
 
 Adafruit_AMG88xx thermalCam;
 HSJsonConnector connector;
 
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
+float max_temps[MOVING_AVG_SIZE];
+
+struct 
 
 void setup() {
   Serial.begin(115200);
@@ -27,21 +34,45 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  delay(100);  // let sensor boot up
 }
 
 void loop() {
-  // read all the pixels
+  // Read all the pixels
   thermalCam.readPixels(pixels);
+
+
 
   String pixels_string;
   for (int i = 0; i < AMG88xx_PIXEL_ARRAY_SIZE; i++) {
-    pixels_string += (pixels_string==NULL ? "" : ",") + String(pixels[i], 2);
+    pixels_string += (pixels_string == NULL ? "" : ",") + String(pixels[i], 2);
   }
 
-  String data = "{\"message\":\"STOVE_HOT\", \"value\":[" + pixels_string + "]}";
+  String data =
+      "[{\"message\":\"STOVE_HOT\", \"value\":[" + pixels_string + "]}]";
   if (true) {
     // TODO: check wifi connection
     connector.send(HSEvent::DATA, data);
   }
   delay(100);
+}
+
+bool stove_hot(float* max_temps) {
+  int count = 0;
+  for (int k = 1; k <= 3; k++) {
+    for (int j = 0; j <= AMG88xx_PIXEL_ARRAY_SIZE - 1; j++) {
+      if (pixels[j] > 30.0) {
+        count = count + 1;
+        break;
+      }
+    }
+    delay(1000);
+    amg.readPixels(pixels);
+  }
+  if (count == 3) {
+    Serial.println("Stove is on");
+  }
+
+  count = 0;
 }
