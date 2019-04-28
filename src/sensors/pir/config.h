6e940f731dbd91478c89289c656f67ba14024a15
justinchuby <justinchuby@users.noreply.github.com>
2019-04-28 14:01:59 -0400

    Add everything to src

--- src/sensors/pir/config.h
+++ src/sensors/pir/config.h
@@ -2,7 +2,7 @@
 
 #define WIFI_SSID "CMU-DEVICE"
 #define WIFI_PASS ""
-#define SENSOR_ID "b78a4d8e-54cb-42f0-9dcb-b223663dfce5"
+#define SENSOR_ID "c76c5d12-6647-11e9-a923-1681be663d3e"
 #define SENSOR_TYPE "grid_eye"
 #define SERVER_URL "http://kinect.andrew.cmu.edu:8000/hs/events"
 #define SERVER_AUTH "Basic aHNfdXNlcjpycGNzX2hzMjAxOQ=="
