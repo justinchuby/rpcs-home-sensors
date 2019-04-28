0a0df115a90c02d65557e77e088aecba776003bf
justinchuby <justinchuby@users.noreply.github.com>
2019-04-28 14:05:56 -0400

    Format code

--- src/sensors/pir/pir.ino
+++ src/sensors/pir/pir.ino
@@ -1,16 +1,16 @@
-#include "config.h"
 #include <HSJsonConnector.h>
+#include "config.h"
 
 // 1 Minute
 #define HANDSHAKE_INTERVAL 60000
 #define REFRESH_RATE 500
 
-int inputPin = 21;               // choose the input pin (for PIR sensor)
-int pirState = LOW;             // we start, assuming no motion detected
-int val = 0;                    // variable for reading the pin status
+int inputPin = 21;   // choose the input pin (for PIR sensor)
+int pirState = LOW;  // we start, assuming no motion detected
+int val = 0;         // variable for reading the pin status
 
 // set up the 'pir0' feed
-//AdafruitIO_Feed *counter = io.feed("pir1");
+// AdafruitIO_Feed *counter = io.feed("pir1");
 
 HSJsonConnector connector;
 int time_count_handshake = 0;
@@ -18,9 +18,9 @@ int time_count_handshake = 0;
 void setup() {
   connector.setSensor(SENSOR_ID, SENSOR_TYPE);
   connector.setServer(SERVER_URL);
-  #ifdef SERVER_AUTH
+#ifdef SERVER_AUTH
   connector.setServerAuth(SERVER_AUTH);
-  #endif
+#endif
   // declare sensor as input
   pinMode(inputPin, INPUT);
 
@@ -39,8 +39,8 @@ void loop() {
   // it should always be present at the top of your loop
   // function. it keeps the client connected to
   // io.adafruit.com, and processes any incoming data.
-//  io.run();
-  
+  //  io.run();
+
   val = digitalRead(inputPin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
     if (pirState == LOW) {
@@ -48,9 +48,8 @@ void loop() {
       pirState = HIGH;
       sendData(1);
     }
-  } 
-  else {
-    if (pirState == HIGH){
+  } else {
+    if (pirState == HIGH) {
       Serial.println("Motion ended!");
       pirState = LOW;
       sendData(0);
@@ -93,6 +92,4 @@ void connectWifi(const char* ssid, const char* password) {
   }
 }
 
-void sendHandshake() {
-  connector.send(HSEvent::HANDSHAKE, "[]");
-}
+void sendHandshake() { connector.send(HSEvent::HANDSHAKE, "[]"); }
