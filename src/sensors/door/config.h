3f077798ee05dc698cf24e770fdd8776f799859d
justinchuby <justinchuby@users.noreply.github.com>
2019-04-28 14:19:23 -0400

    Update door code

---
+++ src/sensors/door/config.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "CMU-DEVICE"
+#define WIFI_PASS ""
+#define SENSOR_ID "505d642b-730f-41ad-a5ee-2dfc04a71b71"
+#define SENSOR_TYPE "door"
+#define SERVER_URL "http://kinect.andrew.cmu.edu:8000/hs/events"
+#define SERVER_AUTH "Basic aHNfdXNlcjpycGNzX2hzMjAxOQ=="
